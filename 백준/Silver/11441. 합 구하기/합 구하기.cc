05172cc4ac15b76322674ca3053050cf3805de51
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2024-12-20 20:27:25 +0900

    [Silver III] Title: 합 구하기, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.cc
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -10,21 +10,20 @@ int main() {
     int N; cin >> N;
 
     int sum = 0;
-    vector<int> v(N + 1, 0); // 누적합을 저장할 벡터, 크기는 N+1
+    vector<int> v(N + 1, 0);
 
     // 누적합 계산
     for (int i = 1; i <= N; i++) {
         int num; cin >> num;
         sum += num;
-        v[i] = sum;  // v[i]에는 1부터 i까지의 누적합을 저장
+        v[i] = sum;  
     }
 
     int M; cin >> M;
     
-    // M번 구간 합을 계산하여 출력
     while (M--) {
         int a, b; cin >> a >> b;
-        cout << v[b] - v[a - 1] << "\n";  // 구간 [a, b]의 합은 v[b] - v[a-1]
+        cout << v[b] - v[a - 1] << "\n";
     }
 
     return 0;
