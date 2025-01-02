acef44e13aa4992372f6ea93b0146fc49a0b49e8
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-01-02 14:52:21 +0900

    [level 2] Title: 시소 짝꿍, Time: 5268.37 ms, Memory: 6.55 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+
+    sort(weights.begin(),weights.end());
+    
+    for(int i=0;i<weights.size();i++)
+    {
+        for(int j = i+1; j < weights.size();j++)
+        {   
+            if((weights[i] == weights[j]) || (weights[i] * 3 == weights[j] * 2)  || (weights[i] * 4 == weights[j] * 3) || (weights[i] * 2 == weights[j]))
+            {
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
