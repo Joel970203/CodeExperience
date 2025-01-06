1439b5714fde721bcadbbedd53993254c39ad387
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-01-06 20:19:59 +0900

    [level 2] Title: 미로 탈출, Time: 0.35 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -1,25 +1,26 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
-#include <iostream>
 #include <cstring>
-bool visited[100][100] = { false };
 using namespace std;
+
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-int cnt;
 
-
-int bfs_end(int x, int y, const vector<string>& maps)
+int bfs(const vector<string>& maps, int startX, int startY, char target)
 {
+    int n = maps.size();
+    int m = maps[0].size();
+    bool visited[100][100] = { false };
     queue<pair<int, int>> q;
-    q.push({ x,y });
-    visited[x][y] = true;
-    cnt = 0;
+    q.push({ startX, startY });
+    visited[startX][startY] = true;
+    int cnt = 0;
+
     while (!q.empty())
     {
         int size = q.size(); // 현재 큐에 남아있는 요소들의 개수를 저장 (한 레벨에서 한 번에 다가가기)
+        
         for (int k = 0; k < size; k++) // 한 레벨씩 처리
         {
             int tempX = q.front().first;
@@ -31,13 +32,13 @@ int bfs_end(int x, int y, const vector<string>& maps)
                 int curX = tempX + dx[i];
                 int curY = tempY + dy[i];
 
-                if (curX >= 0 && curY >= 0 && curX < maps.size() && curY < maps[0].size() && !visited[curX][curY])
+                if (curX >= 0 && curY >= 0 && curX < n && curY < m && !visited[curX][curY])
                 {
-                    if (maps[curX][curY] != 'X')
+                    if (maps[curX][curY] != 'X') // 'X'는 벽, 지나갈 수 없음
                     {
-                        if (maps[curX][curY] == 'E')
+                        if (maps[curX][curY] == target) // 목표에 도달하면
                         {
-                            return cnt + 1; // 목표 'E'에 도달하면 cnt 반환 (여기서 바로 리턴)
+                            return cnt + 1; // 목표 지점에 도달한 최단 경로를 리턴
                         }
                         q.push({ curX, curY });
                         visited[curX][curY] = true;
@@ -45,75 +46,45 @@ int bfs_end(int x, int y, const vector<string>& maps)
                 }
             }
         }
-        cnt++; // 한 번에 큐에서 하나의 레벨을 처리하고 나서 카운트 증가
+        cnt++; // 한 레벨을 다 처리하고 나서 카운트 증가
     }
-    return -1;
+    return -1; // 목표 지점에 도달할 수 없다면 -1 리턴
 }
-
-int bfs_lever(int x, int y, const vector<string>& maps)
-{
-    queue<pair<int, int>> q;
-    q.push({ x,y });
-    visited[x][y] = true;
-    cnt = 0;
-    while (!q.empty())
-    {
-        int size = q.size(); // 현재 큐에 남아있는 요소들의 개수를 저장 (한 레벨에서 한 번에 다가가기)
-        for (int k = 0; k < size; k++) // 한 레벨씩 처리
-        {
-            int tempX = q.front().first;
-            int tempY = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++)
-            {
-                int curX = tempX + dx[i];
-                int curY = tempY + dy[i];
-
-                if (curX >= 0 && curY >= 0 && curX < maps.size() && curY < maps[0].size() && !visited[curX][curY])
-                {
-                    if (maps[curX][curY] != 'X')
-                    {
-                        if (maps[curX][curY] == 'L')
-                        {
-                            return cnt + 1; // 목표 'L'에 도달하면 cnt 반환 (여기서 바로 리턴)
-                        }
-                        q.push({ curX, curY });
-                        visited[curX][curY] = true;
-                    }
-                }
-            }
-        }
-        cnt++; // 한 번에 큐에서 하나의 레벨을 처리하고 나서 카운트 증가
-    }
-    return -1;
-}
-
 
 int solution(vector<string> maps) {
     int c1 = 0;
     int c2 = 0;
+    int startX = 0, startY = 0, leverX = 0, leverY = 0, exitX = 0, exitY = 0;
+
     for (int i = 0; i < maps.size(); i++)
     {
         for (int j = 0; j < maps[0].size(); j++)
         {
             if (maps[i][j] == 'S')
             {
-                c1 = bfs_lever(i, j, maps);
-                if (c1 == -1)
-                    return -1;
-
-                memset(visited, false, sizeof(visited));
+                startX = i;
+                startY = j;
             }
-
-            if (maps[i][j] == 'L')
+            else if (maps[i][j] == 'L')
             {
-                c2 = bfs_end(i, j, maps);
-                if (c2 == -1)
-                    return -1;
-                memset(visited, false, sizeof(visited));
+                leverX = i;
+                leverY = j;
+            }
+            else if (maps[i][j] == 'E')
+            {
+                exitX = i;
+                exitY = j;
             }
         }
     }
+
+    // S -> L
+    c1 = bfs(maps, startX, startY, 'L');
+    if (c1 == -1) return -1;
+
+    // L -> E
+    c2 = bfs(maps, leverX, leverY, 'E');
+    if (c2 == -1) return -1;
+
     return c1 + c2;
 }
