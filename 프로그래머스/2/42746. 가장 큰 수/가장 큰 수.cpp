e401a8a44796cbc8f8f32fd00a5ccfc68fe9c966
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-01-15 20:16:55 +0900

    [level 2] Title: 가장 큰 수, Time: 92.65 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// 비교 함수: 두 문자열을 붙였을 때 더 큰 수가 되도록 비교
+// 비교 함수
 bool compare(string a, string b) {
     return a + b > b + a;
 }
@@ -13,21 +13,20 @@ bool compare(string a, string b) {
 string solution(vector<int> numbers) {
     vector<string> numStrings;
 
-    // 정수를 문자열로 변환하여 벡터에 저장
+   
     for (int num : numbers) {
         numStrings.push_back(to_string(num));
     }
 
-    // 사용자 정의 비교 함수를 사용하여 정렬
+
     sort(numStrings.begin(), numStrings.end(), compare);
 
-    // 정렬된 문자열을 하나로 합침
+
     string result;
     for (string num : numStrings) {
         result += num;
     }
 
-    // 숫자 '0'으로만 구성된 경우를 처리
     if (result[0] == '0') {
         return "0";
     }
@@ -37,10 +36,10 @@ string solution(vector<int> numbers) {
 
 int main() {
     vector<int> numbers = { 6, 10, 2 };
-    cout << solution(numbers) << endl; // 출력: "6210"
+    cout << solution(numbers) << endl; 
 
     numbers = { 3, 30, 34, 5, 9 };
-    cout << solution(numbers) << endl; // 출력: "9534330"
+    cout << solution(numbers) << endl; 
 
     return 0;
 }
