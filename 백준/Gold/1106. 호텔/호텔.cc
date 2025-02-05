f23beeedbcd2a0513f6256fa8b67c5b4001c7fe4
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-02-05 20:38:39 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -10,29 +10,27 @@ int main()
     cin >> C >> N;
 
     vector<pair<int, int>> cities(N); 
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++)
     {
         cin >> cities[i].first >> cities[i].second;
     }
 
-    
-    vector<int> dp(C + 101, INF);
+    vector<int> dp(C+101, INF);
     dp[0] = 0;
 
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++)
     {
-        int cost = cities[i].first;
-        int customers = cities[i].second;
+        int price = cities[i].first;
+        int number = cities[i].second;
 
-        for (int j = customers; j < dp.size(); j++) 
+        for (int j = number; j < dp.size(); j++)
         {
-            dp[j] = min(dp[j], dp[j - customers] + cost);
+            dp[j] = min(dp[j], dp[j - number] + price);
         }
     }
 
     int result = INF;
-    for (int i = C; i < dp.size(); i++) 
-    {
+    for (int i = C; i < dp.size(); i++) {
         result = min(result, dp[i]);
     }
 
