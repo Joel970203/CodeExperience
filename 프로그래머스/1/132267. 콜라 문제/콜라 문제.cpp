8e02ed8791d8e7a3bd7e04687b674e6272ca72c8
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-04-14 10:37:36 +0900

    [level 1] Title: 콜라 문제, Time: 0.06 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -3,14 +3,20 @@
 
 using namespace std;
 
-int solution(int a, int b, int n) {
+int solution(int a, int b, int n) 
+{
+    // 빈 병의 개수 n 
+    // 콜라를 받기위해 마트에 주어야하는 병수 a
+    // 빈 병 a개를 가져다 주면 주는 콜라 병수 b
     int answer = 0;
-
-    while (n >= a) 
-    { 
-        int extra = (n / a) * b; 
-        answer += extra;
-        n = (n % a) + extra;
+    
+    while(n>=a)
+    {
+        n -= a;
+        answer += b;
+        n += b;
     }
+    
     return answer;
+
 }
