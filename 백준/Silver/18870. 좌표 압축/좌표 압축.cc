c4e3a609ee119c11647c113c614ed6d47e6523cf
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-05-22 16:54:08 +0900

    [Silver II] Title: 좌표 압축, Time: 1200 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,64 +1,50 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 #include <unordered_map>
-#define fastio() ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 
 using namespace std;
 
-
-/*
-n^2 이면 10^12 이라 넘어간다.
-
-i를 만났을때, 나는 이 배열에서 j번쨰로 큰 아이입니다 
-라고 알 수 있어야함.
-
-즉 순서.. ?
-
-
-             
-*/
 int main()
 {
+	// 백만개
+	// 범위는 -10억 ~ 10억 (int)
+
+	// N^2 불가능.
+	
 	int N;
 	cin >> N;
-	vector<int> arr;
-	priority_queue<int,vector<int>,greater<>> pq;
-	unordered_map<int, int> map;
-	while (N--)
+	vector<int> arr(N);
+
+	for (int i = 0; i < N; i++)
 	{
-		int k;
-		cin >> k;
-		arr.push_back(k);
-		pq.push(k);
+		cin >> arr[i];
 	}
 
-
-	int temp;
-	int cur = pq.top();
-	int cnt = 0;
-	map[cur] = cnt;
-	pq.pop();
-
-
-	while (!pq.empty())
+	vector<int> temp = arr;
+	sort(temp.begin(), temp.end());
+	unordered_map<int, int> map;
+	
+	int recent = temp[0];
+	int key = 0;
+	map[recent] = 0;
+	for (int i = 1; i < temp.size(); i++)
 	{
-		int temp = pq.top();
-		if (temp == cur)
+		if (temp[i] > recent) // 전 숫자보다 클때 
 		{
-			map[temp] = cnt;
+			recent = temp[i];
+			key++;
+			map[temp[i]] = key;
 		}
 
 		else
 		{
-			cnt++;
-			map[temp] = cnt;
+			map[temp[i]] = key;
 		}
-
-		cur = temp;
-		pq.pop();
+		
 	}
 
-	for (int i = 0; i < arr.size(); i++)
+	for (int i = 0; i < N; i++)
 	{
 		cout << map[arr[i]] << " ";
 	}
