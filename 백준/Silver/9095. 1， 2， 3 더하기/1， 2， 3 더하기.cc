4b5dd0b3c5654c0fa08cbf5758a41640ff4ba9ba
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-08-21 20:10:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,43 +1,28 @@
 #include <iostream>
 #include <vector>
-#define fastio() ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
 using namespace std;
-
-
-
-int DP(int N)
-{
-	if (N == 1)
-	{
-		return 1;
-	}
-
-	else if (N == 2)
-	{
-		return 2;
-	}
-
-	else if (N == 3)
-	{
-		return 4;
-	}
-
-	else
-	{
-		return DP(N - 3) + DP(N - 2) + DP(N - 1);
-	}
-
-}
 
 int main()
 {
-	fastio();
-	int T, N;
-	cin >> T;
-	for (int i = 0; i < T; i++)
+	int n,k;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
 	{
-		cin >> N;
-		cout << DP(N) << endl;
-		
+		cin >> k;
+		vector<int> dp(k + 1);
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 4; i <= k; i++)
+		{
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+		}
+
+		cout << dp[k] << endl;
 	}
+
 }
