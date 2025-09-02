8c9b61ddb2f888372576d71b54e46f620c9fc8d6
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-09-02 15:29:12 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 436 ms, Memory: 8176 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,29 +1,30 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() 
+int main()
 {
-    int n;
-    cin >> n;
-    vector<int> vec;
-    for (int i = 0; i < n; i++) 
-    {
-        int temp;
-        cin >> temp;
-        // vec이 비어 있거나, 마지막 원소보다 현재 값이 클 경우
-        if (vec.empty() || vec.back() < temp) 
-        {
-            vec.push_back(temp);  // 새로운 값 추가
-        }
-        else 
-        {
-            // lower_bound로 적절한 위치를 찾아서 값을 갱신
-            *lower_bound(vec.begin(), vec.end(), temp) = temp;
-        }
-    }
-    cout << vec.size() << endl;  // LIS의 길이 출력
-    return 0;
+	int N;
+	cin >> N;
+	vector<int> arr;
+	int idx = 0;
+	for (int i = 0; i < N; i++)
+	{
+		int k;
+		cin >> k;
+		if (arr.empty() || arr.back() < k)
+		{
+			arr.push_back(k);
+			idx++;
+		}
+
+		else
+		{
+			*lower_bound(arr.begin(), arr.end(), k) = k;
+		}
+	}
+
+	cout << arr.size() << endl;
 }
