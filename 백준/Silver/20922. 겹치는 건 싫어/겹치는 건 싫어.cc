d6c7e2338e24f64267f038486b3885ecd13b4441
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2025-10-15 12:40:16 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 16 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -2,7 +2,8 @@
 #include <vector>
 using namespace std;
 
-int main() {
+int main() 
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -12,15 +13,17 @@ int main() {
     vector<int> arr(N);
     for (int i = 0; i < N; i++) cin >> arr[i];
 
-    vector<int> cnt(100001, 0); // arr 값 범위: 1~100000
+    vector<int> cnt(100001, 0);
 
     int ans = 0;
     int left = 0;
-    for (int right = 0; right < N; right++) {
+
+    for (int right = 0; right < N; right++)
+    {
         cnt[arr[right]]++;
 
-        // 만약 arr[right]가 K를 넘으면 → 조건 위반
-        while (cnt[arr[right]] > K) {
+        while (cnt[arr[right]] > K)
+        {
             cnt[arr[left]]--;
             left++;
         }
@@ -28,5 +31,5 @@ int main() {
         ans = max(ans, right - left + 1);
     }
 
-    cout << ans << "\n";
+    cout << ans;
 }
