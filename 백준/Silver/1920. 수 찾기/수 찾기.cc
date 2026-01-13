97f62c59bf1c9ecc2df95dbcd4892c8a20ae40e8
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2026-01-13 18:48:13 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,61 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> // for sort
+#include <algorithm>
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
 using namespace std;
 
-// 이분탐색을 직접 구현한 함수
-bool binarySearch(const vector<int>& arr, int target) {
-    int low = 0;
-    int high = arr.size() - 1;
+bool my_bs(const vector<int>& arr, int num)
+{
+    int start = 0;
+    int end = (int)arr.size() - 1;
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
 
-        if (arr[mid] == target) {
-            return true; // target을 찾으면 true 반환
-        }
-        else if (arr[mid] < target) {
-            low = mid + 1; // target이 mid보다 크면 오른쪽 절반 탐색
-        }
-        else {
-            high = mid - 1; // target이 mid보다 작으면 왼쪽 절반 탐색
-        }
+        if (arr[mid] == num) return true;
+        else if (num > arr[mid]) start = mid + 1;
+        else end = mid - 1;
     }
-
-    return false; // target을 찾지 못하면 false 반환
+    return false;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+    fastio();
 
-    int N;
+    int N, M;
     cin >> N;
 
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
+    vector<int> arr;
+    arr.reserve(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int k;
+        cin >> k;
+        arr.push_back(k);
     }
 
-    sort(A.begin(), A.end()); // 배열 A를 정렬
+    sort(arr.begin(), arr.end());
 
-    int M;
     cin >> M;
 
-    vector<int> B(M);
-    for (int i = 0; i < M; ++i) {
-        cin >> B[i];
+    for (int i = 0; i < M; i++)
+    {
+        int k;
+        cin >> k;
+        cout << (my_bs(arr, k) ? 1 : 0) << '\n';
     }
-
-    for (int i = 0; i < M; ++i) {
-        // 배열 B의 원소가 배열 A에 존재하는지 직접 구현한 이분탐색을 사용하여 확인
-        if (binarySearch(A, B[i])) {
-            cout << "1\n"; // 존재하면 1
-        }
-        else {
-            cout << "0\n"; // 존재하지 않으면 0
-        }
-    }
-
     return 0;
 }
