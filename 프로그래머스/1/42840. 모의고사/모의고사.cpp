baf49b54577f27e09a58f90df98c618dbd4f77d3
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2026-01-13 18:53:22 +0900

    [level 1] Title: 모의고사, Time: 0.05 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,31 +1,27 @@
-#include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> first = {1,2,3,4,5};
-    vector<int> second = {2,1,2,3,2,4,2,5};
-    vector<int> third = {3,3,1,1,2,2,4,4,5,5};
-    vector<int> cnt(3);
-    for(int i=0;i<answers.size();i++)
-    {
-        if(answers[i] == first[i% first.size()]) cnt[0]++;
-        if(answers[i] == second[i% second.size()]) cnt[1]++;
-        if(answers[i] == third[i% third.size()]) cnt[2]++;
-        
+
+    vector<int> p1 = {1,2,3,4,5};
+    vector<int> p2 = {2,1,2,3,2,4,2,5};
+    vector<int> p3 = {3,3,1,1,2,2,4,4,5,5};
+
+    int s1 = 0, s2 = 0, s3 = 0;
+
+    for (int i = 0; i < (int)answers.size(); i++) {
+        if (answers[i] == p1[i % p1.size()]) s1++;
+        if (answers[i] == p2[i % p2.size()]) s2++;
+        if (answers[i] == p3[i % p3.size()]) s3++;
     }
-    
-    int max_num = *max_element(cnt.begin(),cnt.end());
-    
-    for(int i=0;i<3;i++)
-    {
-        if(cnt[i] == max_num)
-        {
-            answer.push_back(i+1);
-        }
-    }
+
+    int mx = max({s1, s2, s3});
+
+    if (s1 == mx) answer.push_back(1);
+    if (s2 == mx) answer.push_back(2);
+    if (s3 == mx) answer.push_back(3);
+
     return answer;
 }
