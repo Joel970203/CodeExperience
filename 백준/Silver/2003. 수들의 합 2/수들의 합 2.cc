56a476c361486187d0fe6d79958b9a7d27485994
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2026-01-21 17:12:39 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,29 +1,50 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main() {
-    int N, M;
-    cin >> N >> M;
-    
-    vector<int> A(N + 1, 0);  
-    for (int i = 1; i <= N; ++i) {
-        cin >> A[i];
-        A[i] += A[i - 1];  
-    }
-    
-    int count = 0;
-    int left = 0;
-    
-    for (int right = 1; right <= N; ++right) {
-        while (left < right && A[right] - A[left] > M) {
-            left++;
-        }
-        if (A[right] - A[left] == M) {
-            count++;
-        }
-    }
-    
-    cout << count << endl;
-    return 0;
+int main()
+{
+	int N, M;
+	// 이건 슬라이딩 윈도우 
+	cin >> N >> M;
+	vector<int> arr(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int start = 0;
+	int sum = 0;
+	int ans = 0;
+	for (int i = 0; i < N; i++)
+	{
+		/*
+		정답일때,
+		값이 넘어갈때,
+		부족할때
+		*/
+		sum += arr[i];
+
+
+		while (sum > M)
+		{
+			sum -= arr[start];
+			start++;
+			if (sum <= M)
+			{
+				break;
+			}
+		}
+
+
+		if (sum == M)
+		{
+			ans++;
+		}
+	}
+
+	cout << ans;
+	return 0;
 }
