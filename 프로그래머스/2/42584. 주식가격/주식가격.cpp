93010f357f6ffad5547b2d1d6f96680bc4800ffa
Joel970203 <121085543+Joel970203@users.noreply.github.com>
2026-01-22 19:28:10 +0900

    [level 2] Title: 주식가격, Time: 27.28 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,32 +1,67 @@
 #include <string>
 #include <vector>
 #include <stack>
-
 using namespace std;
 
-vector<int> solution(vector<int> prices)
+/*
+직관적으로는 stack을 사용해야할것 같은 감이 오는데 
+
+stack에 1이 있다고 가정 
+2가 들어오면 top하고 비교 
+top 보다 크면 그냥 push
+
+top보다 작으면? pop하고 그위치는 1
+그리고 그 다음 top하고 비교, 작다면 while문 
+크다면 멈춤 마지막 원소도 똑같이 비교하고, 끝났을떄 
+pop연속으로 하면서 벡터 채우기 
+*/
+
+vector<int> solution(vector<int> prices) 
 {
-    int n = prices.size();
-    vector<int> answer(n);
-    stack<int> st;
-
-    for (int i = 0; i < n; ++i) 
+    int cnt = prices.size();
+    vector<int> answer(cnt,0);
+    stack<pair<int,int>> st;
+    
+    for(int i=0; i<cnt; ++i)
     {
-        while (!st.empty() && prices[st.top()] > prices[i])
+        if(st.empty())
         {
-            int idx = st.top();
-            st.pop();
-            answer[idx] = i - idx;
+            st.push({prices[i],i});
         }
-        st.push(i);
+        
+        else
+        {
+            if(prices[i] >= st.top().first) // 안떨어진 경우 
+            {
+                st.push({prices[i],i});
+            }
+            
+            else // 떨어진 경우 
+            {
+                while(!st.empty() &&prices[i] < st.top().first)
+                {
+                    int idx = st.top().second;
+                    answer[idx] = i - idx;
+                    st.pop();
+                }
+                st.push({prices[i],i});
+            }
+        }
     }
-
-    while (!st.empty()) 
+    
+    
+    int time = -1;
+    while(!st.empty())
     {
-        int idx = st.top();
+        if(time == -1)
+        {
+            time = st.top().second;
+        }
+        
+        int idx = st.top().second;
+        answer[idx] = time - idx;
         st.pop();
-        answer[idx] = n - 1 - idx;
     }
-
+    
     return answer;
 }
